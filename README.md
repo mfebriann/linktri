e29431a61e342e8e926e6c14cabce575222e3c86
mfebriann <muhammadfebriyan86@gmail.com>
2022-07-11 22:05:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple website like Linktree
+
+<p> You can modify the appearance easily and as you like. To change the appearance you can change it in the style.css file and you can change it as you like, such: </p>
+<code>background-image</code>, <code>font-size</code>, <code>color</code>, etc.
+
+<p> To change the profile data you can change it from profile.json file in the src folder. You can change your photo, your name, your job/title, etc.</p>
---
+++ app.js
@@ -0,0 +1,72 @@
+// Load content and add class show of element main
+window.addEventListener('load', function () {
+	const main = document.querySelector('main');
+	main.classList.add('show');
+	getDataProfile('src/profile.json');
+});
+
+// Get data profile from profile.json
+function getDataProfile(url) {
+	return fetch(url)
+		.then((repsonse) => repsonse.json())
+		.then((datas) => {
+			const profile = datas.Profile;
+			const container = document.querySelector('main');
+			container.innerHTML = printData(profile);
+			console.log(profile);
+		})
+		.catch((e) => {
+			document.body.innerHTML = `<h1 style="text-align:center; color: white;">${e}</h1>`;
+		});
+}
+
+function printData(profile) {
+	return `
+			<section>
+				<!-- Your profile -->
+				<div class="profile">
+					<!-- Your Photo -->
+					<img src="${profile.photo}" alt="${profile.name}" class="photo-profile" />
+
+					<div class="name-and-title">
+						<!-- Your name -->
+						<p class="name">${profile.name}</p>
+						<!-- Your job/title -->
+						<p class="title">${profile.title}</p>
+					</div>
+				</div>
+
+				<!-- Your Links -->
+				<div class="container-links">${buttonLinks(profile['button-links'])}</div>
+
+				<!-- Your Social Media -->
+				<div class="all-social-media">
+					<p>Connect with me:</p>
+
+					<div class="container-social-media">${socialMedia(profile['social-media'])}</div>
+				</div>
+			</section>
+		`;
+}
+
+function buttonLinks(links) {
+	return links
+		.map((buttonLink) => {
+			return `<a href="${buttonLink.link}" title="${buttonLink['name-link']}" class="btn-link">
+						${buttonLink.icon}
+						${buttonLink['name-link']}
+					</a>`;
+		})
+		.join('');
+}
+
+function socialMedia(links) {
+	return links
+		.map((socialMediaLink) => {
+			console.log(socialMediaLink);
+			return `<a href="${socialMediaLink.link}" title="${socialMediaLink['name-link']}" class="social-media">
+							${socialMediaLink.icon}
+						</a>`;
+		})
+		.join('');
+}
